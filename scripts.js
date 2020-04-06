a3a3cc44d9d57813d58bd8626118fdafc38fd18a
Mrmi <nikola.31023@gmail.com>
2020-04-06 22:41:09 +0200

    Fixed encryption errors (bad logic in algorithm), mergedn encrypt and decrypt functions into one function useCipher.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 var outputParagraph = document.getElementById("outputArea");
 
-function encrypt()
+function useCipher(shouldEncrypt)
 {
   var input = document.getElementById("inputArea").value;
   var inputLength=input.length;
-  var encrypted = "";
+  var message = "";
   var step = parseInt(document.getElementById("step").value);
   var indexOfCurrent;
   for(var i=0; i<inputLength; ++i)
@@ -16,61 +16,46 @@ function encrypt()
     //If current character is in the alphabet
     if(indexOfCurrent>-1)
     {
-      var shifted = alphabet[(indexOfCurrent+step)%26];
+      var shifted;
 
+      if(shouldEncrypt==true)
+      {
+        /*If the input is getting encrypted just move step characters forward through the alphabet
+        (%26 because we can step over the length of the alphabet)*/
+        shifted = alphabet[(indexOfCurrent+step)%26];
+      }
+      else
+      {
+        //If the input is getting decrypted go step characters back through the alphabet
+        if(indexOfCurrent-step<0)
+        {
+          //If we drop below the index 0, just get the alphabet's character at index 26-step
+          shifted = alphabet[26-step];
+        }
+        else
+        {
+          //If we stay above -1, get the curent character's index in the alphabet - step (characters we wnent back)%26
+          shifted = alphabet[(indexOfCurrent-step)%26];
+        }
+      }
+
+      /*Check if the actual inputted character is in upper case:
+      once the character is shifted and set the shifted character to upper case if needed*/
       if(input[i]!==input[i].toLowerCase())
       {
-        /*Check if the actual inputted character is in upper case:
-        once the character is shifted and set the shifted character to upper case if needed*/
+
         shifted=shifted.toUpperCase();
       }
 
-      //Finally add the shifted character to the encrypted message
-      encrypted+=shifted;
+      //Finally add the shifted character to the message
+      message+=shifted;
     }
     else
     {
-      //If the current character isn't in the alphabet just add it to the encrypted message
-      encrypted+=input[i];
+      //If the current character isn't in the alphabet just add it to the message
+      message+=input[i];
     }
   }
-  //Set the output paragraph's text to the encrypted message
-  outputParagraph.innerHTML = encrypted;
-}
-
-function decrypt()
-{
-  var input = document.getElementById("inputArea").value;
-  var inputLength=input.length;
-  var decrypted = "";
-  var step = parseInt(document.getElementById("step").value);
-  var indexOfCurrent;
-  for(var i=0; i<inputLength; ++i)
-  {
-    //Conver current character to lower case and find its index in the alphabet
-    indexOfCurrent = alphabet.indexOf(input[i].toLowerCase());
-
-    //If current character is in the alphabet
-    if(indexOfCurrent>-1)
-    {
-      var shifted = alphabet[(indexOfCurrent-step)%26];
-
-      if(input[i]!==input[i].toLowerCase())
-      {
-        /*Check if the actual inputted character is in upper case:
-        once the character is shifted and set the shifted character to upper case if needed*/
-        shifted=shifted.toUpperCase();
-      }
-
-      //Finally add the shifted character to the decrypted message
-      decrypted+=shifted;
-    }
-    else
-    {
-      //If the current character isn't in the alphabet just add it to the decrypted message
-      decrypted+=input[i];
-    }
-  }
-  //Set the output paragraph's text to the decrypted message
-  outputParagraph.innerHTML = decrypted;
+  //Set the output paragraph's text to the message
+  outputParagraph.innerHTML = message;
 }
